1b41b6fd8e38192f15a26239869480a6d63e9cc6
srpandey <Srijan.Pandey@misys.com>
2017-06-19 09:05:09 +0530

    adding few more JS Functions

--- javascript/bleh.js
+++ javascript/bleh.js
@@ -1,8 +1,7 @@
 
 var searchEnabled = false; 
 var childNumber = 1; 
-
-
+var searchString=""; 
 
 function toggleSearchEnabled() {
 	return searchEnabled == true ? false : true; 
@@ -18,9 +17,7 @@ function onButtonClick() {
 		$('.search-button').animate({left: '+=100' } , 500 );
 		$('.search-box').animate({opacity: 0} , 1000).css({visibility: 'hidden'}); 
 	}
-
 	searchEnabled = toggleSearchEnabled(); 
-	
 }
 
 
@@ -40,12 +37,42 @@ function onTextChange() {
 
 /**
 initiate all wiki related calls 
-
 **/
 function initiateWikipediaSearch() {
 	console.log("WikiPedia Search started "); 
+	//captureSearchTextValue();
+	moveSearchToTop(); 
+	//useWikiRestApi(searchString);  
 	createContainers();
 }
+
+ /*
+ Capture text value in a variable 
+  */
+function captureSearchTextValue() {
+	// searchString = $(".search-box").val(); 
+	// console.log(searchString);
+}
+
+
+/*
+will move the search button and textfield to
+top of the view 
+*/
+function moveSearchToTop() {
+	//hide current search box 
+	$(".search-black-box").animate({opacity: '0.0'} , "slow");
+
+
+}
+
+/**
+Consumes the rest api for 
+wikipedia and gets all result set 
+*/ 
+// function useWikiRestApi(var searchValue ) {
+
+// }
 
 
 /**
